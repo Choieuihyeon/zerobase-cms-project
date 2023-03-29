75e933b33af1046d312c485bc9b8ace864cb78b6
CHOI <ceh201312@naver.com>
2023-03-29 12:42:05 +0900

    초기 파일 commit

---
+++ src/main/java/com/zerobase/cms/zerobasecmsproject/ZerobaseCmsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.cms.zerobasecmsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerobaseCmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerobaseCmsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/cms/zerobasecmsproject/ZerobaseCmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.cms.zerobasecmsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerobaseCmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
